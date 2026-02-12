2475da17f1eecbeade2522abc83352a18e834ad4
Attiq <attiqbashir0@gmail.com>
2026-02-12 07:50:11 +0000

    Add ScheduleController integration test setup

---
+++ src/test/java/com/hospital/system/appointments/controller/schedule/ScheduleControllerIntegrationTest.java
@@ -0,0 +1,32 @@
+package com.hospital.system.appointments.controller.schedule;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.WebApplicationContext;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@Transactional
+public class ScheduleControllerIntegrationTest {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setUp(){
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .apply(SecurityMockMvcConfigurers.springSecurity())
+                .build();
+        objectMapper = new ObjectMapper();
+    }
+}
